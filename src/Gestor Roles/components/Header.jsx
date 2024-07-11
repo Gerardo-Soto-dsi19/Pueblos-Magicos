b429555341e1365eca4fe51d538737ee706f1c22
Gerardo Soto Vega <spdsi19@ipn.mx>
2024-07-11 12:21:53 -0600

    Creacion de dropdpwn con la opcion para editar la informacion del usuario

--- src/Gestor Roles/components/Header.jsx
+++ src/Gestor Roles/components/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import { HiDotsVertical } from "react-icons/hi";
+import { FaUserCircle } from 'react-icons/fa';
+import { Dropdown } from 'flowbite-react'
 function Header() {
+    
     return (
         <>
             <header className='bg-white border rounded-md px-4  py-2.5'>
-                <label>Administrador de sistema</label>
+                <div className='flex justify-between mt-1'>
+                    <label>Administrador de sistema</label>
+                    <div className='flex items-center cursor-pointer'>
+                        <Dropdown label="" dismissOnClick={false} renderTrigger={() => <span><HiDotsVertical /></span>}>
+                            <Dropdown.Item>
+                                <FaUserCircle className="inline-block mr-2" />
+                                <span>Editar perfil</span>
+                            </Dropdown.Item>
+                        </Dropdown>
+                    </div>
+                </div>
             </header>
         </>
     )
