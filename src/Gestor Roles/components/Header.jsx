eea075f054b8dd040956c1605d8a80463e4a71ab
Gerardo Soto Vega <spdsi19@ipn.mx>
2024-07-16 18:43:36 -0600

    Creacion de Dropdown para opcion de editar informacion personal en header

--- src/Gestor Roles/components/Header.jsx
+++ src/Gestor Roles/components/Header.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { HiDotsVertical } from "react-icons/hi";
 import { FaUserCircle } from 'react-icons/fa';
 import { Dropdown } from 'flowbite-react'
+import { Link } from "react-router-dom"
 function Header() {
-    
+
     return (
         <>
             <header className='bg-white border rounded-md px-4  py-2.5'>
@@ -12,8 +13,10 @@ function Header() {
                     <div className='flex items-center cursor-pointer'>
                         <Dropdown label="" dismissOnClick={false} renderTrigger={() => <span><HiDotsVertical /></span>}>
                             <Dropdown.Item>
-                                <FaUserCircle className="inline-block mr-2" />
-                                <span>Editar perfil</span>
+                                <Link to={'/editar/informacion-personal'}>
+                                    <FaUserCircle className="inline-block mr-2" />
+                                    <span>Editar perfil</span>
+                                </Link>
                             </Dropdown.Item>
                         </Dropdown>
                     </div>
