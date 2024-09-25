7e9197bdfdf2822b790dbdf21ee1c7b26fe2b076
Gerardo Soto Vega <spdsi19@ipn.mx>
2024-09-25 17:32:23 -0600

    Se modifica la validacion para ocultar la opcion del gestor de roles, segun el tipo de usuario

--- src/Gestor Roles/PerfilUsuario.jsx
+++ src/Gestor Roles/PerfilUsuario.jsx
@@ -9,12 +9,16 @@ function PerfilUsuario() {
     const menuItems = [
         { icon: <RiAddBoxFill className="mr-2" />, label: "Registrar nueva publicación", to: "/formulario/registro" },
         { icon: <FaBook className="mr-2" />, label: "Gestor de publicaciones", to: "/gestor-solicitudes" },
-        { icon: <FaLock className="mr-2" />, label: "Gestor de roles", to: "/gestor-roles", 
-          condition: sessionStorage.getItem("tu") === "1" || sessionStorage.getItem("tu") === "2" },
+        {
+            icon: <FaLock className="mr-2" />, label: "Gestor de roles", to: "/gestor-roles",
+            condition: sessionStorage.getItem("tu") === "1" || sessionStorage.getItem("tu") === "2"
+        },
     ];
+    
+    const filteredMenuItems = menuItems.filter(item => !item.hasOwnProperty('condition') || item.condition);
 
     return (
-        <Sidebar menuItems={menuItems.filter(item => !item.condition || item.condition)}>
+        <Sidebar menuItems={filteredMenuItems}>
             <Header />
             <div className='mt-14'>
                 <EditarInformacion idUsuario={localStorage.getItem('user_name')} />
